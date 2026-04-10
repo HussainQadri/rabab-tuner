513f2e1f6edf01309b3145aed4f36a57f5b46ca5
HussainQadri <hussain.bqadri@gmail.com>
2026-04-10 01:24:38 +0100

    use flash-socketio to run its own server, don't rely on gunicorn as it terminates worker due to watchdog timer

--- app.py
+++ app.py
@@ -171,4 +171,7 @@ def analyze_audio():
 
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    debug = os.environ.get("FLASK_DEBUG", "0") == "1"
+    socketio.run(app, host="0.0.0.0", port=port, debug=debug)
